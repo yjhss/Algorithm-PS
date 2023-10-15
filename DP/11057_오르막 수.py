614735ff676254977a5e406a8acbc86e633bdf7b
yjhss <20211420.sungshin.ac.kr>
2023-10-15 20:24:08 +0900

    #1 11057_오르막 수

---
+++ DP/11057_오르막 수.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [[0] * 10 for i in range(1001)]
+
+for i in range(10):
+    dp[1][i] = 1
+
+for i in range(2, n + 1):
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = 1
+        else:
+            dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+print(sum(dp[n]) % 10007)
