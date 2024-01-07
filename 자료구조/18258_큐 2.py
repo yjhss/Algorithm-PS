28d01267746af68c6b93560448d8af9af0d0abd8
yjhss <20211420.sungshin.ac.kr>
2024-01-07 22:11:48 +0900

    #3 18258_큐 2

---
+++ 자료구조/18258_큐 2.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline())
+
+queue = deque([])
+
+for i in range(N):
+    command = sys.stdin.readline().strip().split()
+    
+    if command[0] == 'push':
+        queue.append(command[1])
+        
+    elif command[0] == 'pop':
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+    elif command[0] == 'size':
+        print(len(queue))
+    
+    elif command[0] == 'empty':
+        if queue:
+            print(0)
+        else:
+            print(1)
+    elif command[0] == 'front':
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+    elif command[0] == 'back':
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
